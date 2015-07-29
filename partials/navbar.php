10d1c851a257f9c0c139890be87342b973df4a92
Bhargv Panth <bhargavrpanth@gmail.com>
2015-07-29 15:31:55 +0530

    First commit

---
+++ partials/navbar.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <title>V-Evaluator</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </head>
+
+  <body>
+    <div class="container-fluid">
+        <ul class="nav navbar-inverse">
+            <li class="text-right"><a href="#" style="background-color:#1C1C1C;"><span class="glyphicon glyphicon-off" aria-hidden="true" style="color:#FFF;"></span></a></li>
+        </ul>
