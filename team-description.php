f1162da79f281f24a0e3f2aae15d50f9418b5600
Gururaj Dharani <dharani.gururaj@gmail.com>
2015-07-30 13:52:03 +0530

    Minor changes

---
+++ team-description.php
@@ -0,0 +1,11 @@
+<?php require 'partials/navbar.php' ?>        
+        <ul class="nav navbar-default">
+            <li class="text-center"><h4 style="text-color:#353535;">THEMES</h4></li>
+        </ul>
+    
+        <!-- cover ends here -->
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js" type="text/javascript"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/script.js"></script>
+    </html>
+</body>
