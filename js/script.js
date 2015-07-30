9f8866f9759ecf2fe8332dba3fbe741fe6389fa1
Gururaj Dharani <dharani.gururaj@gmail.com>
2015-07-30 18:05:28 +0530

    Made changes

--- js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('a[rel="relativeanchor"]').click(function(){
-        $('html, body').animate({
-            scrollTop: $( $.attr(this, 'href') ).offset().top
-        }, 1000);
-        return false;
-    });
-});
-
